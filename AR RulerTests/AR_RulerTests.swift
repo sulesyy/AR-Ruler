f2cbbc1b24b042c192f5dddd21ed5bd6939603bc
suleyilmaz22 <2303013099@student.beykent.edu.tr>
2026-02-17 17:51:17 +0300

    Initial commit - AR Ruler App

---
+++ AR RulerTests/AR_RulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_RulerTests.swift
+//  AR RulerTests
+//
+//  Created by Şule Yılmaz on 24.12.2025.
+//
+
+import Testing
+@testable import AR_Ruler
+
+struct AR_RulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
